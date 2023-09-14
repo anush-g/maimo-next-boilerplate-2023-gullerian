006ce25c0661bce6264eb52dd4f99ddd069bd869
nushi-g <gullerian.a@gmail.com>
2023-09-14 18:58:28 -0300

    changed cards

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -1,17 +1,40 @@
 import Image from 'next/image';
 import ButtonLink from './ButtonLink';
+import Link from 'next/link';
 
 const ShowCard = ({ actualShow }) => {
   const { show } = actualShow;
-  const { name, id, image } = show;
+  const { name, id, image, premiered, summary, webChannel } = show;
+  const summaryBien = summary?.replace(/<\/?p>|<\/?b>/g, '');
+
   return (
-    <div className='show_container col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 flex flex-col gap-3 justify-start items-center'>
-      <div className='border border-slate-800 border-solid mb-2 w-full h-auto'>
-        <Image src={image?.original} width={680} height={1000} alt={name} />
+    <Link className='w-96' href={`/show/${id}`}>
+      <div className='p-4 '>
+        <div className='h-full border-2 border-rose-200 border-opacity-60 rounded-lg overflow-hidden'>
+          <Image className='lg:h-96 md:h-96 w-full object-cover object-center' width={680} height={1000} alt={name} src={image?.original} />
+          <div className='p-6 bg-rose-100'>
+            <h2 className='tracking-widest text-xs title-font font-medium text-gray-400 mb-1'>{premiered?.substring(0, 4)|| '-'}</h2>
+            <h1 className='title-font text-lg font-bold text-rose-500 mb-3'>{name}</h1>
+            <p className='leading-relaxed mb-3'>{summaryBien?.substring(0, 130) + '...'|| '-'}</p>
+            <div className='flex items-center flex-wrap '>
+              <a className='text-rose-800 inline-flex items-center md:mb-2 lg:mb-0'>Ver más
+                <svg className='w-4 h-4 ml-2' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2' fill='none' stroke-linecap='round' stroke-linejoin='round'>
+                  <path d='M5 12h14'></path>
+                  <path d='M12 5l7 7-7 7'></path>
+                </svg>
+              </a>
+
+              <span className='text-gray-400  lg:ml-auto md:ml-0 ml-auto inline-flex items-center leading-none text-sm'>
+              <svg className='w-4 h-4 mr-1' stroke='currentColor' stroke-width='2' fill='none' stroke-linecap='round' stroke-linejoin='round' viewBox='0 0 24 24'>
+                  <path d='M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z'></path>
+                  <circle cx='12' cy='12' r='3'></circle>
+                </svg>{webChannel?.name || '-'}
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
-      <h3>{name}</h3>
-      <ButtonLink id={id} />
-    </div>
+      </Link>
   );
 };
 
--- src/containers/CardsContainer.js
+++ src/containers/CardsContainer.js
@@ -4,18 +4,20 @@ import { useAppContext } from '@/contexts/AppContext';
 const CardsContainer = () => {
   const { shows, loading } = useAppContext();
   return (
-    <section>
-      <h2>Shows</h2>
+    <section className="my-16">
+      <h2 className="text-4xl">Movies</h2>
       {!loading && (
-        <div className='inner my-0 mx-auto max-w-[1200px] px-4'>
-          <div className='grid grid-cols-12 gap-4 gap-y-6'>
-            {shows.map((actualShow, index) => {
-              return <ShowCard actualShow={actualShow} key={index} />;
-            })}
+        <section class="text-gray-600 body-font">
+          <div class="container px-5 py-12 mx-auto">
+            <div class="flex flex-wrap -m-4 justify-center">
+              {shows.map((actualShow, index) => {
+                return <ShowCard actualShow={actualShow} key={index} />;
+              })}
+            </div>
           </div>
-        </div>
+        </section>
       )}
-      {loading && <p className='w-full flex justify-center'>Loading...</p>}
+      {loading && <p className="w-full flex justify-center">Loading...</p>}
     </section>
   );
 };
